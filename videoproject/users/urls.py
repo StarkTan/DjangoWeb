e1157d3c34dc8b7798864f5c189990734f099407
Stark <386707933@qq.com>
2019-12-17 21:18:26 +0800

    学习视屏点播网站搭建：个人中心页

--- videoproject/users/urls.py
+++ videoproject/users/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
+    path('profile/<int:pk>/', views.ProfileView.as_view(), name='profile'),
+    path('change_password/', views.change_password, name='change_password'),
+    path('subscribe/<int:pk>/', views.SubscribeView.as_view(), name='subscribe'),
+    path('feedback/', views.FeedbackView.as_view(), name='feedback'),
+
+
 ]
--- videoproject/users/views.py
+++ videoproject/users/views.py
@@ -1,7 +1,20 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-from .forms import SignUpForm, UserLoginForm
-from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.urls import reverse
+from django.views import generic
+
+from helpers import AuthorRequiredMixin
+from ratelimit.decorators import ratelimit
+
+from .forms import SignUpForm, UserLoginForm, ProfileForm, ChangePwdForm, SubscribeForm, FeedbackForm
+from .models import User, Feedback
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout, update_session_auth_hash
+
+
 # Create your views here.
+
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -42,3 +55,64 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('home')
+
+
+class ProfileView(LoginRequiredMixin,AuthorRequiredMixin, generic.UpdateView):
+    model = User
+    form_class = ProfileForm
+    template_name = 'users/profile.html'
+
+    def get_success_url(self):
+        messages.success(self.request, "保存成功")
+        return reverse('users:profile', kwargs={'pk': self.request.user.pk})
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = ChangePwdForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            if not user.is_staff and not user.is_superuser:
+                user.save()
+                update_session_auth_hash(request, user)  # 更新session 非常重要！
+                messages.success(request, '修改成功')
+                return redirect('users:change_password')
+            else:
+                messages.warning(request, '无权修改管理员密码')
+                return redirect('users:change_password')
+        else:
+            print(form.errors)
+    else:
+        form = ChangePwdForm(request.user)
+    return render(request, 'registration/change_password.html', {
+        'form': form
+    })
+
+
+class SubscribeView(LoginRequiredMixin,AuthorRequiredMixin, generic.UpdateView):
+    model = User
+    form_class = SubscribeForm
+    template_name = 'users/subscribe.html'
+
+    def get_success_url(self):
+        messages.success(self.request, "保存成功")
+        return reverse('users:subscribe', kwargs={'pk': self.request.user.pk})
+
+
+class FeedbackView(LoginRequiredMixin, generic.CreateView):
+
+    model = Feedback
+    form_class = FeedbackForm
+    template_name = 'users/feedback.html'
+
+    @ratelimit(key='ip', rate='2/m')
+    def post(self, request, *args, **kwargs):
+        was_limited = getattr(request, 'limited', False)
+        if was_limited:
+            messages.warning(self.request, "操作太频繁了，请1分钟后再试")
+            return render(request, 'users/feedback.html', {'form': FeedbackForm()})
+        return super().post(request, *args, **kwargs)
+
+    def get_success_url(self):
+        messages.success(self.request, "提交成功")
+        return reverse('users:feedback')
