c3746f81175f0c3e1dedfdd7c8c4ebd1c36e7bb9
Stark <386707933@qq.com>
2019-12-26 21:15:45 +0800

    DRF ：移除quickstart

--- rest_demo/rest_demo/urls.py
+++ rest_demo/rest_demo/urls.py
@@ -14,18 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
-from rest_framework import routers
-from quickstart import views
 from django.contrib import admin
 from django.conf.urls import include
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-
-    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),  # 在页面右上角增加登录选项
     path('', include('snippets.urls')),
