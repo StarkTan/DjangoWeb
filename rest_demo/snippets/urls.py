b0d67b01ce6391484b94bc8df51c39b493293864
Stark <386707933@qq.com>
2019-12-26 19:29:57 +0800

    DRF ：使用ViewAPI生成视图

--- rest_demo/snippets/urls.py
+++ rest_demo/snippets/urls.py
@@ -4,8 +4,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    # path('snippets/', views.snippet_list),
+    # path('snippets/<int:pk>/', views.snippet_detail),
+
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)  # 返回一个 URL pattern 列表，其中包含附加到每个 URL pattern 的格式后缀模式
+
