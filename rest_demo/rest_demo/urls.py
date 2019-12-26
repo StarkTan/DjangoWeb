d2c9132dde50e18f674abedc2a50a6adede0d549
Stark <386707933@qq.com>
2019-12-26 17:56:04 +0800

    DRF ：使用序列化

--- rest_demo/rest_demo/urls.py
+++ rest_demo/rest_demo/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
 
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('snippets.urls')),
 ]
