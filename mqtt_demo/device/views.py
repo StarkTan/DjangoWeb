58d1c30ff0647b06f9d8da2c54c14b288093c27f
Stark <386707933@qq.com>
2020-01-08 14:32:18 +0800

    修改 device config 时同时推送修改消息

--- mqtt_demo/device/views.py
+++ mqtt_demo/device/views.py
@@ -6,12 +6,25 @@ from .models import Device
 from .serializers import DeviceSerializer
 # Create your views here.
 from rest_framework import viewsets, mixins
+from mqtt_client.mqtt_config import client
 
+pub_pre = '/usi/cloud/config/'
 
 class DeviceViewSet(mixins.UpdateModelMixin,
                      viewsets.ReadOnlyModelViewSet):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
+
+    def update(self, request, *args, **kwargs):
+        super().update(request, *args, **kwargs)
+
+    def perform_update(self, serializer):
+        serializer.validated_data['confirm'] = False
+        serializer.save()
+        sn = serializer.data['id']
+        config = serializer.data['config']
+        # 推送数据
+        client.publish(pub_pre + sn, config, qos=1)
 
     @action(methods=['GET'],detail=True)
     def config(self, request, *args, **kwargs):
