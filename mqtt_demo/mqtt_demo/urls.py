76c8f8679c3687867cdc6aac2c5b0c2d44910dd4
Stark <386707933@qq.com>
2020-01-09 20:19:56 +0800

    修改导入BUG

--- mqtt_demo/mqtt_demo/urls.py
+++ mqtt_demo/mqtt_demo/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mqtt_client import mqtt_config  # 启动mqtt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
